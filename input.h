052105474da7f704b9a06e79540cab235b085f7e
Md. Aryaan Raza <254004041+AryaanRaza@users.noreply.github.com>
2026-01-22 12:41:56 +0530

    v2.0: Modular structure with input validation and edge-case handling

---
+++ input.h
@@ -0,0 +1,7 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+int isNumber(char *str);
+int safeInputInt(const char *prompt, int min, int max);
+
+#endif
--- student.c
+++ student.c
@@ -16,63 +16,15 @@ Author  : Md Aryaan Raza
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+
 #include "student.h"
+#include "input.h"
 
 #define SUBJECTS 3
 
 
 
-int isNumber(char *str)
-{
-    if (str[0] == '\0') // empty input
-        return 0;
 
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return 0; // false, not a number
-    }
-
-    return 1; // true, valid number
-}
-
-int safeInputInt(const char *prompt, int min, int max)
-{
-    char input[100];
-    int value = 0, len, i;
-
-    while (1)
-    {
-        printf("%s", prompt);
-        // Input from user as string to avoid crashing if user gives string instead of int.
-
-        fgets(input, sizeof(input), stdin);
-
-        // Remove new line.
-        len = strlen(input);
-        if (len > 0 && input[len - 1] == '\n')
-            input[len - 1] = '\0';
-
-        // Check if number is Valid
-        if (!isNumber(input))
-        {
-            printf("Invalid input! Please enter a valid integer.\n");
-            continue;
-        }
-
-        // Convert to Integer
-        value = atoi(input);
-
-        // Checks Rangs
-        if (value < min || value > max)
-        {
-            printf("Please enter a number between %d and %d.\n", min, max);
-            continue; // ask again
-        }
-        return value; // valid input, return it
-    }
-}
 
 int searchByRoll(struct Student *stu, int count, int roll)
 {
